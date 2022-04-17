a024953c28b89f6a41df2659aa6e9cb84cf51c66
Arron Vinyard <joinedsenses@gmail.com>
2022-04-16 20:09:44 -0400

    Update constantcrawler.py

--- Packages/SourcePawn/constantcrawler.py
+++ Packages/SourcePawn/constantcrawler.py
@@ -1,106 +1,146 @@
-import sys
 import os.path
-import re
-from trieregex import TrieRegEx as TRE # https://github.com/ermanh/trieregex
 import pyperclip
+import re
+import sys
+from io import TextIOWrapper
+from trieregex import TrieRegEx # https://github.com/ermanh/trieregex
 
 DEBUG = False
 
-"""
-Crawls through include directories to generate a regex trie for matching constants.
-Ignores variables IN_ALL_CAPS since those are already discovered with a basic all-caps regex for the
-purpose of syntax highlighting.
-"""
-if len(sys.argv) < 2:
-    print("Expected one or more parameters for include directory path ({})".format(len(sys.argv)))
-    for arg in sys.argv:
-        print(arg)
-    sys.exit(1)
+def main():
+    """
+    Crawls through include directories to generate a regex trie for matching constants.
+    Ignores variables IN_ALL_CAPS since those are already discovered with a basic all-caps regex for the
+    purpose of syntax highlighting.
+    """
+    if len(sys.argv) < 2:
+        print("Expected one or more parameters for include directory path ({})".format(len(sys.argv)))
+        sys.exit(1)
 
-tre = TRE()
+    trie = TrieRegEx()
 
-for i in range(1, len(sys.argv)):
-    include_dir = sys.argv[i]
+    # loop through all args and attempt to parse
+    for i in range(1, len(sys.argv)):
+        arg = sys.argv[i]
 
-    if not os.path.isdir(include_dir):
-        print('Path not found: {}'.format(include_dir))
-        continue
-
-    # loop through each file in the directory
-    for file_name in os.listdir(include_dir):
-        # if it's not a .inc file, skip it
-        if not file_name.endswith('.inc'):
+        # if arg is file and ends with .inc, then parse it
+        if os.path.isfile(arg):
+            if arg.endswith('.inc'):
+                with open(arg) as f: parse_file(f, trie)
             continue
 
-        code = ''
+        if not os.path.isdir(arg):
+            print('Path not found: {}'.format(arg))
+            continue
 
-        # open and read the file
-        with open(os.path.join(include_dir, file_name)) as f:
-            if DEBUG: print('-- Reading from {} --'.format(file_name))
+        # loop through each file in the directory
+        for file_name in os.listdir(arg):
+            # if it's not a .inc file, skip it
+            if not file_name.endswith('.inc'): continue
 
-            code = f.read()
+            # open and read the file
+            with open(os.path.join(arg, file_name)) as f: parse_file(f, trie)      
 
-            # remove multi-line comments
-            code = re.sub(r'\/\*(.|\n)*?\*\/', '', code)
+    result = trie.regex()
+    if DEBUG: print(result)
 
-            #remove single-line comments
-            code = re.sub(r'\/\/.*\n', '', code)
+    pyperclip.copy(result)
+    print("Output copied to clipboard (Len: {})".format(len(result)))
 
-            # scoop the innards from all enums, excluding enum structs
-            for enum_innards_match in re.finditer(r'enum(?!\s+struct)(?:.|\n)*?{((?:.|\n)*?)}', code):
-                enum_innards = enum_innards_match.group(1)
+def parse_file(f: TextIOWrapper, trie: TrieRegEx) -> None:
+    """
+    Parses a file for constants and adds them to the regex trie
 
-                # try to get each enum variable
-                for enum_def_match in re.finditer(r'(?:^|\n)\s*?(\w+)\b', enum_innards):
-                    def_text = enum_def_match.group(1)
+    Args:
+        f (TextIOWrapper): The file to parse
+        tre (TrieRegEx): The trie to add results to.
+    """
+    if DEBUG: print('-- Reading from {} --'.format(f.name))
 
-                    # if it's all uppercase, skip it
-                    if (re.match(r'\b[A-Z_\d]+\b', def_text)):
-                        continue
+    code = f.read()
 
-                    # skip if already contains
-                    if (tre.has(def_text)):
-                        if DEBUG: print('Skipping enum, already added: {}'.format(def_text))
-                        continue
+    # remove multi-line comments
+    code = re.sub(r'\/\*(.|\n)*?\*\/', '', code)
 
-                    tre.add(def_text)
-                    if DEBUG: print('Enum added: {}'.format(def_text))
+    #remove single-line comments
+    code = re.sub(r'\/\/.*$', '', code)
 
-            # match all defines
-            for define_match in re.finditer(r'^#define[ \t]*(\w+)\b[ \t]', code):
-                define = define_match.group(1)
+    parse_enums(code, trie)
 
-                # if all uppercase, ignore. Typically they should be uppercase but maybe there's an exception
-                if (re.match(r'\b[A-Z_\d]+\b', define)):
-                    continue
-                
-                # skip if already contains
-                if (tre.has(define)):
-                    if DEBUG: print('Skipping define, already added: {}'.format(define))
-                    continue
+    parse_defines(code, trie)
 
-                tre.add(define)
-                if DEBUG: print('Define added: {}'.format(define))
+    parse_publicconstants(code, trie)
 
-            # match public constants aka magic variables
-            for constant_match in re.finditer(r'public[ \t]+const[ \t]+\w+[ \t]+(\w+)\b', code):
-                constant = constant_match.group(1)
 
-                # if all uppercase, ignore
-                if (re.match(r'\b[A-Z_\d]+\b', constant)):
-                    continue
+def parse_enums(code: str, trie: TrieRegEx) -> None:
+    """
+    Finds enum values and adds them to the regex trie
 
-                # skip if already contains
-                if (tre.has(constant)):
-                    if DEBUG: print('Skipping const, already added: {}'.format(constant))
-                    continue
+    Args:
+        code (str): Text to search through.
+        trie (TrieRegEx): Trie to add results to.
+    """
+    # scoop the innards from all enums, excluding enum structs
+    for enum_innards_match in re.finditer(r'enum(?!\s+struct)(?:.|\n)*?{((?:.|\n)*?)}', code):
+        if DEBUG: print('-- Enum match: --\n{}\n-------'.format(enum_innards_match.group(0)))
+        enum_innards = enum_innards_match.group(1)
 
-                tre.add(constant)
-                if DEBUG: print('Const added: {}'.format(constant))
+        # try to get each enum variable
+        for enum_def_match in re.finditer(r'(?:^|\n)\s*?(\w+)\b', enum_innards):
+            def_text = enum_def_match.group(1)
 
-result = tre.regex()
+            # if it's all uppercase, skip it
+            if (re.match(r'\b[A-Z_\d]+\b', def_text)): continue
 
-if DEBUG: print(result)
+            # skip if already contains
+            if (trie.has(def_text)):
+                if DEBUG: print('Skipping enum, already added: {}'.format(def_text))
+                continue
 
-pyperclip.copy(result)
-print("Output copied to clipboard (Len: {})".format(len(result)))
+            trie.add(def_text)
+            if DEBUG: print('Enum added: {}'.format(def_text))
+
+
+def parse_defines(code: str, trie: TrieRegEx) -> None:
+    """
+    Finds defines and adds them to the regex trie
+
+    Args:
+        code (str): Text to search through.
+        trie (TrieRegEx): Trie to add results to.
+    """
+    # match all defines
+    for define_match in re.finditer(r'^#define[ \t]*(\w+)\b[ \t]', code):
+        define = define_match.group(1)
+
+        # if all uppercase, ignore. Typically they should be uppercase but maybe there's an exception
+        if (re.match(r'\b[A-Z_\d]+\b', define)): continue
+        
+        # skip if already contains
+        if (trie.has(define)):
+            if DEBUG: print('Skipping define, already added: {}'.format(define))
+            continue
+
+        trie.add(define)
+        if DEBUG: print('Define added: {}'.format(define))
+
+
+def parse_publicconstants(code: str, trie: TrieRegEx) -> None:
+    # match public constants aka magic variables
+    for constant_match in re.finditer(r'public[ \t]+const[ \t]+\w+[ \t]+(\w+)\b', code):
+        constant = constant_match.group(1)
+
+        # if all uppercase, ignore
+        if (re.match(r'\b[A-Z_\d]+\b', constant)): continue
+
+        # skip if already contains
+        if (trie.has(constant)):
+            if DEBUG: print('Skipping const, already added: {}'.format(constant))
+            continue
+
+        trie.add(constant)
+        if DEBUG: print('Const added: {}'.format(constant))
+
+# -------------
+
+main()
